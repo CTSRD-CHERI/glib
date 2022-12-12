8f1ddad1b0ab6b3e8980ebb0e62fcf1801ef5884
Alex Richardson <arichardson@FreeBSD.org>
2022-12-12 21:58:30 +0000

    GAtomicArray: Store the metadata in a guintptr
    This unbreaks the code for CHERI where using gsize results in values that
    are only aligned to 8 bytes, but we need 16 byte alignment for pointers.

--- gobject/gatomicarray.h
+++ gobject/gatomicarray.h
@@ -27,7 +27,7 @@
 
 G_BEGIN_DECLS
 
-#define G_ATOMIC_ARRAY_DATA_SIZE(mem) (*((gsize *) (mem) - 1))
+#define G_ATOMIC_ARRAY_DATA_SIZE(mem) (*((guintptr *) (mem) - 1))
 
 typedef struct _GAtomicArray GAtomicArray;
 struct _GAtomicArray {
